eac053f919d3f1c971bd5c8287e24d7446b656ea
Arvinth Vijayanathan <arvinth3@hotmail.com>
2019-04-04 08:43:39 -0400

    Added navigating between two main pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'Bezuem',
+    home: MyApp(),
+    theme: ThemeData(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+final ThemeData themeData = ThemeData(
+  canvasColor: Colors.lightGreenAccent,
+  accentColor: Colors.black,
+  primarySwatch: Colors.green,
+);
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+  Widget build(BuildContext ctx) {
+    //TODO: Mathusan
+    return Scaffold(
+      backgroundColor: Theme.of(ctx).canvasColor,
+      body: Center(
+          child: FlatButton(
+              color: Colors.green,
+              onPressed: () {
+                Navigator.push(ctx, SpotifyAuthPage());
+              },
+              child: Text("Enter Playlist URL"))),
     );
   }
+}
+
+class SpotifyAuthPage extends MaterialPageRoute<Null> {
+  SpotifyAuthPage()
+      : super(builder: (BuildContext ctx) {
+          return Scaffold(
+              body: Center(
+            child: RaisedButton(
+                onPressed: () {
+                  Navigator.pop(ctx);
+                },
+                child: Text("Sign into Spotify")),
+          ));
+        });
 }
 
 class MyHomePage extends StatefulWidget {
